8da04b1c2d601e6c4cd7b0bb54eec2ba14c6d93b
Timothy Flach <tmflach@cpp.edu>
2022-10-05 19:22:10 -0700

    Initial outline of intended program.

---
+++ TicTacToe.c
@@ -0,0 +1,24 @@
+//Tic Tac Toe game between 2 players or player vs computer.
+
+
+int main() {
+    //prompt user for game they wish to play.
+
+    //read information from console
+
+    //start a game vs players or vs computer based on input.
+
+    //display end game message. Win, lose, or tie.
+    return 0;
+}
+
+//create function for reading player input format: #row #column
+
+//create function to run 2 player game.
+
+//create function to run human vs random computer game.
+
+//create function to check end game condition.
+
+//create function to display the board to player(s).
+
